cd0d1d0efae737a4ab8ab5268c8db630cc8b7d6a
rmoreno162303 <105377377+rmoreno162303@users.noreply.github.com>
2022-08-15 16:49:47 -0500

    update for dynamic ports
    dynamic ports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
